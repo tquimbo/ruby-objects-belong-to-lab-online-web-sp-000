2240d5c652e79b5856f2a9897701ab7a6512e828
Tim Quimbo <tquimbo1@gmail.com>
2021-03-11 17:34:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
 
-    attr_accessor :name, :genre, :song
+  attr_accessor :name, :genre, :song
 
   def initialize(name, genre, song)
     @name = name
@@ -8,9 +8,6 @@ class Artist
     @song = song
   end
 
-  drake = Artist.new("Drake", "rap")
-  hotline_bling = Song.new("Hotline Bling")
 
-  hotline_bling.artist = drake
   
 end
