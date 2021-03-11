b9a8d94a6ffa5f11d877a597a108c1a68c6f49ed
Tim Quimbo <tquimbo1@gmail.com>
2021-03-11 17:33:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,9 +8,9 @@ class Artist
     @song = song
   end
 
+  drake = Artist.new("Drake", "rap")
+  hotline_bling = Song.new("Hotline Bling")
+
+  hotline_bling.artist = drake
+  
 end
-
-drake = Artist.new("Drake", "rap")
-hotline_bling = Song.new("Hotline Bling")
-
-hotline_bling.artist = drake
